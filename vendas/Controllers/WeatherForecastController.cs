42051a3d8e525bdba22b58e3492fae13c0b7c8fb
Bruna Leonel <psbru2012@gmail.com>
2023-01-12 15:39:57 -0300

    criacao de pastas do projeto

--- vendas/Controllers/WeatherForecastController.cs
+++ vendas/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using vendas.Models;
 
 namespace vendas.Controllers
 {
--- vendas/Models/WeatherForecast.cs
+++ vendas/Models/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace vendas
+namespace vendas.Models
 {
     public class WeatherForecast
     {
